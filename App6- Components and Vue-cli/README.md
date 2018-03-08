b1d0a0335e3ade5b425162dc4b6e42c6321d0481
latimercaleb <nohitokaze@gmail.com>
2018-03-08 08:15:39 -0500

    Adding commented template breakdown

--- App6- Components and Vue-cli/README.md
+++ App6- Components and Vue-cli/README.md
@@ -18,4 +18,4 @@ npm run build
 For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
 
 ## Tasks
-Using simple components, seperate and add layered functionality to current code
+Using simple components, separate and add layered functionality to current code
--- App6- Components and Vue-cli/src/App.vue
+++ App6- Components and Vue-cli/src/App.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <header>
+                <header> <!--Component 1-->
                     <h1>Server Status</h1>
                 </header>
             </div>
@@ -10,21 +10,21 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
+                <ul class="list-group"> <!--Component 2-->
                     <li
                             class="list-group-item"
                             v-for="index in 5">
-                        Server #{{ index }}
+                        Server #{{ index }} <!--Component 3-->
                     </li>
                 </ul>
             </div>
-            <div class="col-xs-12 col-sm-6">
+            <div class="col-xs-12 col-sm-6"> <!--Component 4-->
                 <p>Server Details are currently not updated</p>
             </div>
         </div>
         <hr>
         <div class="row">
-            <div class="col-xs-12">
+            <div class="col-xs-12"> <!--Component 5-->
                 <footer>
                     <p>All Servers are managed here</p>
                 </footer>
