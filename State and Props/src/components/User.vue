3b30debe6ae35447a1cb7f1753c574f9ac1a5f0e
latimercaleb <nohitokaze@gmail.com>
2018-03-21 23:09:44 -0400

    Props in vue started

--- State and Props/src/components/User.vue
+++ State and Props/src/components/User.vue
@@ -2,10 +2,11 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button v-on:click = "changeName">Change the Name</button>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail :name="parentName"></app-user-detail><!--This passes parentName to the child as name-->
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -19,6 +20,16 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+          data: function(){
+                return{
+                      parentName: 'Callat'
+                };
+          },
+          methods:{
+                changeName: function(){
+                      this.parentName =  'latimeks'
+                }
+          },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
--- State and Props/src/components/UserDetail.vue
+++ State and Props/src/components/UserDetail.vue
@@ -2,10 +2,26 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>My name is {{name}} and backwards is {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
+// Parent => Child comms on components
+export default{
+      // Form 1: Array with value name, now it can be passed in by the parent component, recall names can't be in camel or pascal case, can use kebab case.
+      // props: ['name'],
+
+      // Form 2: As an object,. we can provide validation
+      props:{
+            name: [String, Array] // type inference can pass multiple types
+      },
+      methods: {
+            switchName() {
+                  return this.name.split("").reverse().join(""); // reverses string
+            }
+      }
+}
 </script>
 
 <style scoped>
